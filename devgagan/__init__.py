9c3ab22e23581e00ac3999b83cab5c37466eed64
bdfhsgh0-max <bdfhsgh0@gmail.com>
2025-12-04 11:03:06 +0530

    Update __init__.py

--- devgagan/__init__.py
+++ devgagan/__init__.py
@@ -4,7 +4,7 @@
 #              and uploading them back to Telegram.
 # Author: Gagan
 # GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
+# Telegram: https://t.me/+5H5tJB8hlJo2MjU1
 # YouTube: https://youtube.com/@dev_gagan
 # Created: 2025-01-11
 # Last Modified: 2025-01-11
